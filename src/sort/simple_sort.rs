1db6427db29d06a71e5ea78a31b028cf5cd15d09
Duckulus <amin.haddou@outlook.de>
2024-12-06 20:45:22 +0100

    add insertion and bubble sort

--- src/sort/simple_sort.rs
+++ src/sort/simple_sort.rs
@@ -1,9 +1,8 @@
-
 pub fn selection_sort<T: PartialOrd>(array: &mut [T]) {
-    for i in 0..array.len()-1 {
+    for i in 0..array.len() - 1 {
         let mut min = i;
         for j in i + 1..array.len() {
-            if array[j] < array[min]{
+            if array[j] < array[min] {
                 min = j;
             }
         }
@@ -11,12 +10,49 @@ pub fn selection_sort<T: PartialOrd>(array: &mut [T]) {
     }
 }
 
+pub fn insertion_sort<T: PartialOrd>(array: &mut [T]) {
+    for i in 1..array.len() {
+        for j in (1..=i).rev() {
+            if array[j] < array[j - 1] {
+                array.swap(j - 1, j);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+pub fn bubble_sort<T: PartialOrd>(array: &mut [T]) {
+    let mut swapped;
+    for i in 0..array.len() {
+        swapped = false;
+        for j in 0..array.len() - i - 1{
+            if array[j] > array[j+1] {
+                array.swap(j, j+1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+}
+
 #[test]
 pub fn simple_sort_test() {
     let start = [1, 5, 7, 3, 2, 4, 9, 10, 8, 6];
-    let goal = [1,2,3,4,5,6,7,8,9,10];
+    let mut goal = start.clone();
+    goal.sort();
 
     let mut array = start.clone();
     selection_sort(&mut array);
+    assert_eq!(array, goal);
+
+    array = start.clone();
+    insertion_sort(&mut array);
+    assert_eq!(array, goal);
+
+    array = start.clone();
+    bubble_sort(&mut array);
     assert_eq!(array, goal);
 }
